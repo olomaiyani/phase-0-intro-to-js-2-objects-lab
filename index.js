bcd377829caab74be1937e8659942950f41a716a
+ <+>
2022-08-24 14:34:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+const employee={name:"Sam" ,streetAddress:"11 Broadway"}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return { ...employee,[key]:value};
+
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee [key] = value;
+    return employee;
+
+}
+function deleteFromEmployeeByKey(employee,key){
+   const sem = { ...employee};
+   delete sem[key];
+   return sem;
+
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+   delete employee[key];
+   return employee;
+ }
